d896ec5cb7009d5b44f78decf2eca82fbb22e259
Эльнар Мустафин <mustafinelnare@gmail.com>
2023-06-09 20:18:15 +0300

    index update

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <App />
